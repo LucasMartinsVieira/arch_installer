948c30a05a3a23229f42ebc317c667d7b4128a0a
Lucas Martins Vieira <lucasmartvieira03@outlook.com>
2025-01-19 11:48:44 -0300

    updates

--- arch_installer.sh
+++ arch_installer.sh
@@ -257,13 +257,13 @@ aur() {
     read -rp "[+] Do you want to enable the Chaotic AUR? [y/N]: " answer_chaotic
     case "$answer_chaotic" in
     [Yy]*)
-      arch-chroot -u "$username" /mnt sh -c '
+      arch-chroot /mnt sh -c '
       pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
       pacman-key --lsign-key 3056513887B78AEB
       pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
       pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-      echo "[chaotic-aur]" >> /mnt/etc/pacman.conf
-      echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /mnt/etc/pacman.conf
+      echo "[chaotic-aur]" >> /etc/pacman.conf
+      echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
       pacman -Syu
       '
       echo -e "${GREEN}Chaotic AUR enabled${NC}"
